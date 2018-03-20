7df207abd5b590bb6d505abd7f5eaeae1b45865f
carlos <celotts@gmail.com>
2018-03-19 21:44:49 -0430

    Angular 5

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -90,7 +90,7 @@ export class LoginService {
 
   /**
    * Carga Variable de sesion.
-   * @param data
+   * @param data Lott
    */
   cargaVariableSesion(data):void{
     this.aS.cargaVariableSesion(data);
